06a2e1edce42b5e33ef4941ae387845a4c059196
aidarnygmet <aidarn198@gmail.com>
2022-11-16 02:36:05 +0600

    added second activity

--- app/src/main/java/com/example/speedreading/MainActivity.kt
+++ app/src/main/java/com/example/speedreading/MainActivity.kt
@@ -1,104 +1,117 @@
 package com.example.speedreading
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.util.TypedValue
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.slider.Slider
+import com.google.android.material.textfield.TextInputLayout
 import kotlinx.coroutines.*
-import kotlinx.coroutines.delay
-
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var speedText: TextView
-    private lateinit var start: Button
+    private lateinit var start: ImageButton
     private lateinit var textSpeed: Slider
     private lateinit var textSize: Slider
-
-    private var isRunning = false
-    private var isContinuing = true
-    private val textToRead = "Everyone is entitled to all the rights and freedoms set forth in this Declaration, without distinction of any kind, such as race, colour, sex, language, religion, political or other opinion, national or social origin, property, birth or other status. Furthermore, no distinction shall be made on the basis of the political, jurisdictional or international status of the country or territory to which a person belongs, whether it be independent, trust, non-self-governing or under any other limitation of sovereignty."
-    private var listToRead = textToRead.split(" ")
-    private var wordPerMin =300
-    private lateinit var pause: Button
-    private lateinit var layout: ConstraintLayout
-    //var job: Job? = null
+    private lateinit var textInput: EditText
+    private lateinit var submit: Button
+    private var textToRead = ""
 
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        pause = Button(this)
+        submit = findViewById(R.id.submit)
+        textInput = findViewById(R.id.textInput)
         speedText = findViewById(R.id.speedText)
         start = findViewById(R.id.startButton)
         textSpeed = findViewById(R.id.textSpeed)
         textSize = findViewById(R.id.textSize)
-        layout = findViewById(R.id.layout)
-        wordPerMin = textSpeed.value.toInt()
-        var lastWord = 0
-        fun printWords(){
-            GlobalScope.launch {
-                val x= lastWord
-                for (i in x until listToRead.size){
-                    if (isContinuing and isRunning){
-                        runOnUiThread { speedText.text = listToRead[i] }
-                        lastWord = i
-                        delay((60000/wordPerMin).toLong())
-                        if(i == listToRead.size-1){
-                            runOnUiThread { speedText.text = "" }
-                            layout.removeView(pause)
-                            isRunning = false
-                            isContinuing = true
-                            start.text = "Start"
-                            lastWord = 0
-                        }
-                    } else {
-                        break
-                    }
-                }
-            }
+        submit.setOnClickListener {
+            textToRead = textInput.text.toString()
+            Log.d("test", textToRead)
         }
-        start.setOnClickListener {
-            if (isRunning){
-                isRunning = false
-                isContinuing = true
-                start.text = "Start"
-                speedText.text = ""
-                layout.removeView(pause)
-                lastWord = 0
-            } else {
-                Log.d("test", "message1")
-                isRunning = true
-                start.text = "Stop"
-                pause.text="Pause"
-                layout.addView(pause)
-                printWords()
-            }
-        }
-
-        pause.setOnClickListener {
-            if(isContinuing){
-                isContinuing = false
-                pause.text = "continue"
-            } else {
-                isContinuing = true
-                pause.text = "pause"
-                printWords()
-            }
-        }
+        start.setOnClickListener(View.OnClickListener { v: View? ->
+            // Intents are objects of the android.content.Intent type. Your code can send them to the Android system defining
+            // the components you are targeting. Intent to start an activity called SecondActivity with the following code.
+            val intent = Intent(this, reading::class.java)
+            // start the activity connect to the specified class
+            intent.putExtra("text", textToRead)
+            intent.putExtra("speed", textSpeed.value.toInt())
+            intent.putExtra("size", textSize.value)
+            startActivity(intent)
+        })
         textSize.addOnChangeListener{
                 _, value, _ -> speedText.setTextSize(TypedValue.COMPLEX_UNIT_SP, value*50)
         }
-        textSpeed.addOnChangeListener{
-            _, value, _ ->wordPerMin = value.toInt()
-        }
+
     }
+//        var lastWord = 0
+//        fun printWords(){
+//            GlobalScope.launch {
+//                val x= lastWord
+//                for (i in x until listToRead.size){
+//                    if (isContinuing and isRunning){
+//                        runOnUiThread { speedText.text = listToRead[i] }
+//                        lastWord = i
+//                        delay((60000/wordPerMin).toLong())
+//                        if(i == listToRead.size-1){
+//                            runOnUiThread { speedText.text = "" }
+//                            layout.removeView(pause)
+//                            isRunning = false
+//                            isContinuing = true
+//                            start.text = "Start"
+//                            lastWord = 0
+//                        }
+//                    } else {
+//                        break
+//                    }
+//                }
+//            }
+//        }
+//        start.setOnClickListener {
+//            if (isRunning){
+//                isRunning = false
+//                isContinuing = true
+//                start.text = "Start"
+//                speedText.text = ""
+//                layout.removeView(pause)
+//                lastWord = 0
+//            } else {
+//                Log.d("test", "message1")
+//                isRunning = true
+//                start.text = "Stop"
+//                pause.text="Pause"
+//                layout.addView(pause)
+//                printWords()
+//            }
+//        }
+//
+//        pause.setOnClickListener {
+//            if(isContinuing){
+//                isContinuing = false
+//                pause.text = "continue"
+//            } else {
+//                isContinuing = true
+//                pause.text = "pause"
+//                printWords()
+//            }
+//        }
+
+//        textSpeed.addOnChangeListener{
+//            _, value, _ ->wordPerMin = value.toInt()
+//        }
+//    }
 
 }
 
---
+++ app/src/main/java/com/example/speedreading/reading.kt
@@ -0,0 +1,81 @@
+package com.example.speedreading
+
+import android.os.Bundle
+import android.util.TypedValue
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.properties.Delegates
+
+class reading : AppCompatActivity() {
+    private var listToRead = listOf<String>()
+    private var speed = 0
+    private var size: Float = 0.0F
+    private var lastWord = 0
+    private var isRunning = true
+    private lateinit var text: TextView
+    private lateinit var pause: ImageButton
+    private lateinit var stop: ImageButton
+    private lateinit var inc: ImageButton
+    private lateinit var dec: ImageButton
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reading)
+        text = findViewById(R.id.speedText)
+        pause = findViewById(R.id.pauseButton)
+        stop = findViewById(R.id.stopButton)
+        inc = findViewById(R.id.increase)
+        dec = findViewById(R.id.decrease)
+        val extras = intent.extras
+        if (extras != null) {
+            listToRead = extras.getString("text").toString().split(" ")
+            speed = extras.getInt("speed")
+            size = extras.getFloat("size")
+            text.setTextSize(TypedValue.COMPLEX_UNIT_SP, size*50)
+        }
+
+        printWords()
+        pause.setOnClickListener {
+            if(isRunning){
+                isRunning = false
+                pause.setImageResource(R.drawable.play)
+            } else {
+                isRunning = true
+                pause.setImageResource(R.drawable.pause)
+                printWords()
+            }
+        }
+        stop.setOnClickListener {
+            finish()
+        }
+        dec.setOnClickListener {
+            speed-=10
+        }
+        inc.setOnClickListener {
+            speed+=10
+        }
+    }
+    private fun printWords(){
+            GlobalScope.launch {
+                val x = lastWord
+                for (i in x until listToRead.size){
+                    if (isRunning){
+                        runOnUiThread {
+                            text.text = listToRead[i]
+                        }
+                        lastWord = i
+                        delay((60000/speed).toLong())
+                        if(i == listToRead.size-1){
+                            finish()
+                        }
+                    } else {
+                        break
+                    }
+                }
+            }
+        }
+}
